6d579fad932f06f0237e51f4ed980dff9843963c
Egge <egge@my2sats.space>
2025-08-07 14:14:35 +0200

    feat: change to prod mint

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,6 +1,6 @@
 import { CashuMint, CashuWallet } from "@cashu/cashu-ts";
 
-const MINT_URL = "https://testnut.cashu.space";
+const MINT_URL = "https://mint.minibits.cash/Bitcoin";
 
 export const wallet = new CashuWallet(new CashuMint(MINT_URL));
 export { MINT_URL };
