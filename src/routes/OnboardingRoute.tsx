31bf96dc0d8b651b2bd845028c28d1bcb9fdb16b
Egge <egge@my2sats.space>
2025-08-08 08:47:29 +0200

    feat: meta tags

--- src/routes/OnboardingRoute.tsx
+++ src/routes/OnboardingRoute.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { configRepository } from "../database";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
